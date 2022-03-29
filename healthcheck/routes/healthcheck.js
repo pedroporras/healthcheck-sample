d19768e26016a9af9b05475434a31d44b55978be
Pedro Porras <pedro.porras@nmksoftware.co>
2022-03-29 00:32:49 -0500

    feat: Adicion de endpoints para liveness y readiness
    Se agregan endpoints para liveness y readiness usando la libreria nodejs-health-checker

--- healthcheck/routes/healthcheck.js
+++ healthcheck/routes/healthcheck.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const { HealthcheckerDetailedCheck, HealthcheckerSimpleCheck} = require('nodejs-health-checker/dist/healthchecker/healthchecker')
+const { Dialects, HealthTypes } = require('nodejs-health-checker/dist/interfaces/types')
 
-
-router.get('/healthcheck', (req, res) => {
-    res.status(200).send('OK');
+router.get('/liveness', (req, res) => {
+    res.json(HealthcheckerSimpleCheck());
 });
-router.get('/', (req, res) => res.json({message: 'Hellos'}));
+router.get('/readiness', async (req, res) => {
+    let result = await HealthcheckerDetailedCheck({
+        name: 'ReadinessExample',
+        version: '1.0.0',
+        integrations: [
+            {
+                type: HealthTypes.Database,
+                name: "Simple Database integration check",
+                host: "localhost",
+                dbPort: 5432,
+                dbName: "postgres",
+                dbUser: "postgres",
+                dbPwd: "root",
+                dbDialect: Dialects.postgres,
+            },
+            {
+                type: HealthTypes.Web,
+                name: 'Simple API integration check',
+                host: 'https://github.com'
+            }
+        ],
+    });
+    res.json(result);
+});
 
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 server.use(cors());
 server.use('/healthcheck', healthcheckRoutes);
-server.get('/', (req, res) => res.json({ message: 'Hello' }));
+
+server.get('/', (req, res) => {
+    res.json({ status: "I'm alive!" });
+})
 //server.use('/api', apiRoute);
 
 
